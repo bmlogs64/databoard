60bd56937ef9e06727bc4bd5762c2396fe74a813
bmlogs64 <bernardomellogs@gmail.com>
2025-09-24 17:25:04 -0300

    Ajustando requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,20 +1,36 @@
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
-from app.services.data_service import buscar_e_tratar_dados
+from fastapi.middleware.cors import CORSMiddleware
+import os
+from app.data_service import buscar_e_tratar_dados
 
-app = FastAPI(title="Dashboard de Dados")
+app = FastAPI()
+
+origins = [
+    "https://bmlogs64.github.io/databoard",
+    "*" 
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"], 
+    allow_headers=["*"],
 )
 
 @app.get("/dados")
-def get_dados():
+def dados():
     try:
         dados = buscar_e_tratar_dados()
-        return {"status": "sucesso", "total": len(dados), "dados": dados}
+        return dados
     except Exception as e:
         return {"status": "erro", "mensagem": str(e)}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8000)),
+        reload=True
+    )
